95eb0b0dbcd0b3727f16559c50cfaaaec7250e7a
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-11-30 12:40:25 +0900

    ok

--- src/lib/host/getHackableHosts.ts
+++ src/lib/host/getHackableHosts.ts
@@ -5,16 +5,17 @@ export const getHackableHosts = (ns: NS) => {
     const server = ns.getServer(host);
     const homeFiles = ns.ls("home");
 
-    return (
+    const isHigherLevel = ns.getPlayer().skills.hacking >= (server.requiredHackingSkill ?? 0);
+    const openableCount =
       (server.openPortCount ?? 0) +
-        [
-          homeFiles.includes("BruteSSH.exe") && !server.sshPortOpen,
-          homeFiles.includes("FTPCrack.exe") && !server.ftpPortOpen,
-          homeFiles.includes("relaySMTP.exe") && !server.smtpPortOpen,
-          homeFiles.includes("HTTPWorm.exe") && !server.httpPortOpen,
-          homeFiles.includes("SQLInject.exe") && !server.sqlPortOpen,
-        ].filter(Boolean).length >=
-      (server.numOpenPortsRequired ?? 0)
-    );
+      [
+        homeFiles.includes("BruteSSH.exe") && !server.sshPortOpen,
+        homeFiles.includes("FTPCrack.exe") && !server.ftpPortOpen,
+        homeFiles.includes("relaySMTP.exe") && !server.smtpPortOpen,
+        homeFiles.includes("HTTPWorm.exe") && !server.httpPortOpen,
+        homeFiles.includes("SQLInject.exe") && !server.sqlPortOpen,
+      ].filter(Boolean).length;
+
+    return isHigherLevel && openableCount >= (server.numOpenPortsRequired ?? 0);
   });
 };
