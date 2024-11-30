d7df96463193faa27b4d9cd866cf7236b7cb1f4a
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-12-01 02:58:11 +0900

    ok

--- src/lib/host/getHostStats.ts
+++ src/lib/host/getHostStats.ts
@@ -5,7 +5,11 @@ export const getHostStats = (host: Host, ns: NS) => {
     hackAnalyze: ns.hackAnalyze(host),
     hackAnalyzeChance: ns.hackAnalyzeChance(host),
     weakenAnalyze: ns.weakenAnalyze(1),
-    growthAnalyze: ns.growthAnalyze(host, 1),
+    growth: ns.getServerGrowth(host),
+    growthAnalyze: ns.growthAnalyze(host, 300),
     growthAnalyzeSecurity: ns.growthAnalyzeSecurity(1, host),
+    hackTime: ns.getHackTime(host),
+    growTime: ns.getGrowTime(host),
+    weakenTime: ns.getWeakenTime(host),
   };
 };
