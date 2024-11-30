1162a311de00901e27579301fa0efb42a53c6ef1
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-12-01 02:03:47 +0900

    ok

--- src/script/daemons.ts
+++ src/script/daemons.ts
@@ -2,5 +2,5 @@ import { buyTera } from "/lib/tera/buyTera";
 import { repeat } from "/util/repeat";
 
 export async function main(ns: NS): Promise<void> {
-  await repeat(ns, () => buyTera(ns), 2000);
+  await Promise.all([await repeat(ns, () => buyTera(ns), 2000)]);
 }
