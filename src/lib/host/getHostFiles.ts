847bca01260c316058e09f3b8416491dbb159119
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-12-01 00:41:09 +0900

    ok

---
+++ src/lib/host/getHostFiles.ts
@@ -0,0 +1,5 @@
+import { getHackableHosts } from "/lib/host/getHackableHosts";
+
+export const getHostFiles = (ns: NS) => {
+  getHackableHosts(ns).map((host) => ns.scp(ns.ls(host), "home", host));
+};
