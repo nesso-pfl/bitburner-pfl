5ae840e9731ec92b57cd00bc29841c863a1b1da9
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-12-01 22:27:21 +0900

    ok

--- src/script/daemons.ts
+++ src/script/daemons.ts
@@ -1,6 +1,16 @@
+import { getHackableHosts } from "/lib/host/getHackableHosts";
 import { buyTera } from "/lib/tera/buyTera";
+import { getAllTeras } from "/lib/tera/getAllTeras";
+import { copyScripts } from "/util/copyScripts";
 import { repeat } from "/util/repeat";
 
 export async function main(ns: NS): Promise<void> {
-  await Promise.all([await repeat(ns, () => buyTera(ns), 2000)]);
+  await Promise.all([
+    await repeat(ns, () => buyTera(ns), 2000),
+    await repeat(
+      ns,
+      () => [...getHackableHosts(ns), ...getAllTeras(ns)].forEach((server) => copyScripts(ns, server)),
+      5000,
+    ),
+  ]);
 }
