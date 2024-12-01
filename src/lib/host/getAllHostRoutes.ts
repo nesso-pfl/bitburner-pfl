57cd17b0b6cd5e19dc803806bc2c2fe7b593dcea
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-12-01 12:06:00 +0900

    ok

--- src/lib/host/getAllHostRoutes.ts
+++ src/lib/host/getAllHostRoutes.ts
@@ -3,15 +3,15 @@ import { nub } from "/util/nub";
 type Route = Host[];
 
 export const getAllHostRoutes = (ns: NS) => {
-  return getNewRoutes([["home"]], ns);
+  return getNewRoutes(ns, [["home"]]);
 };
 
-const getNewRoutes = (routes: Route[], ns: NS): Route[] => {
+const getNewRoutes = (ns: NS, routes: Route[]): Route[] => {
   const allHosts = nub(routes.flat());
   const newRoutes = routes.flatMap((route) => {
     const newRoutes_ = ns.scan(route.at(-1)).filter((host) => !allHosts.includes(host));
     return newRoutes_.map((host) => [...route, host]);
   });
 
-  return newRoutes.length === 0 ? routes : [...routes, ...getNewRoutes(newRoutes, ns)];
+  return newRoutes.length === 0 ? routes : [...routes, ...getNewRoutes(ns, newRoutes)];
 };
