35331cfa95f9397faaf312dca11b9e5e9e3bda2c
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-12-02 17:09:44 +0900

    ok

---
+++ src/script/hack.ts
@@ -0,0 +1,7 @@
+import { toHost } from "/lib/host/toHost";
+import { arg } from "/util/arg";
+
+export async function main(ns: NS): Promise<void> {
+  const host = toHost(arg(ns.args[0], "string"));
+  await ns.hack(host);
+}
