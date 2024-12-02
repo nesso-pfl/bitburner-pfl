9e777dc2a7c2f35841a3367fb2fb6db4eb0ade93
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-12-02 18:32:37 +0900

    ok

--- src/script/weaken.ts
+++ src/script/weaken.ts
@@ -2,7 +2,6 @@ import { toHost } from "/lib/host/toHost";
 import { arg } from "/util/arg";
 
 export async function main(ns: NS): Promise<void> {
-  ns.tprint("weaken");
   const host = toHost(arg(ns.args[0], "string"));
   await ns.weaken(host);
 }
