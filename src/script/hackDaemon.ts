faf24dd1523a25db5c3818703770586c1b4205e5
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-12-02 18:44:29 +0900

    ok

---
+++ src/script/hackDaemon.ts
@@ -0,0 +1,8 @@
+import { toHost } from "/lib/host/toHost";
+import { arg } from "/util/arg";
+import { repeat } from "/util/repeat";
+
+export async function main(ns: NS): Promise<void> {
+  const host = toHost(arg(ns.args[0], "string"));
+  await repeat(ns, async () => await ns.hack(host), 0);
+}
