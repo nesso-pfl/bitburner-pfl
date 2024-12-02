614e75f7f1e622eb03d7f7e45fc61f5db23421c4
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-12-03 05:14:39 +0900

    ok

--- src/script/growDaemon.ts
+++ src/script/growDaemon.ts
@@ -4,7 +4,7 @@ import { repeat } from "/util/repeat";
 
 export async function main(ns: NS): Promise<void> {
   const host = toHost(arg(ns.args[0], "string"));
-  const stopOnMaxMoney = arg(ns.args[0], "boolean", false);
+  const stopOnMaxMoney = arg(ns.args[1], "boolean", false);
   await repeat(ns, async () => await ns.grow(host), 0, {
     until: (ns: NS) => stopOnMaxMoney && ns.getServerMoneyAvailable(host) === ns.getServerMaxMoney(host),
   });
--- src/script/weakenDaemon.ts
+++ src/script/weakenDaemon.ts
@@ -4,7 +4,7 @@ import { repeat } from "/util/repeat";
 
 export async function main(ns: NS): Promise<void> {
   const host = toHost(arg(ns.args[0], "string"));
-  const stopOnMinSecLevel = arg(ns.args[0], "boolean", false);
+  const stopOnMinSecLevel = arg(ns.args[1], "boolean", false);
   await repeat(ns, async () => await ns.weaken(host), 0, {
     until: (ns: NS) => stopOnMinSecLevel && ns.getServerSecurityLevel(host) === ns.getServerMinSecurityLevel(host),
   });
