74a3a57e6e446ee09a694ecfcb1449a5aba0b3aa
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-12-02 21:49:53 +0900

    ok

--- src/util/repeat.ts
+++ src/util/repeat.ts
@@ -1,5 +1,6 @@
-export const repeat = async (ns: NS, fn: () => unknown, duration: number) => {
+export const repeat = async (ns: NS, fn: () => unknown, duration: number, count?: number) => {
   await fn();
+  if (count === 1) return;
   await ns.sleep(duration);
-  await repeat(ns, fn, duration);
+  await repeat(ns, fn, duration, count === undefined ? count : count - 1);
 };
