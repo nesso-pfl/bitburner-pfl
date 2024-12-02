a6ae58508b3345cbbd6f6c7e5417c6b0175d5a96
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-12-03 06:26:54 +0900

    ok

--- src/lib/host/nuke.ts
+++ src/lib/host/nuke.ts
@@ -1,9 +1,10 @@
 export const nuke = (ns: NS, host: Host) => {
   const server = ns.getServer(host);
-  if (!server.httpPortOpen) ns.httpworm(host);
-  if (!server.ftpPortOpen) ns.ftpcrack(host);
-  if (!server.sqlPortOpen) ns.sqlinject(host);
-  if (!server.sshPortOpen) ns.brutessh(host);
-  if (!server.smtpPortOpen) ns.relaysmtp(host);
+  const homeFiles = ns.ls("home");
+  if (homeFiles.includes("BruteSSH.exe") && !server.sshPortOpen) ns.brutessh(host);
+  if (homeFiles.includes("FTPCrack.exe") && !server.ftpPortOpen) ns.ftpcrack(host);
+  if (homeFiles.includes("relaySMTP.exe") && !server.smtpPortOpen) ns.relaysmtp(host);
+  if (homeFiles.includes("HTTPWorm.exe") && !server.httpPortOpen) ns.httpworm(host);
+  if (homeFiles.includes("SQLInject.exe") && !server.sqlPortOpen) ns.sqlinject(host);
   ns.nuke(host);
 };
