dd01b01c605bc97bac1a9b65aba9f7d25860eefa
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-12-03 06:15:34 +0900

    ok

---
+++ src/lib/port/clearPorts.ts
@@ -0,0 +1,7 @@
+import { handleMasterPort } from "/lib/port/handleMasterPort";
+
+export const clearPorts = (ns: NS) => {
+  const masterPort = handleMasterPort(ns);
+  const newPorts = (masterPort.read() as number[]).filter((port) => !ns.getPortHandle(port).empty());
+  masterPort.write(newPorts);
+};
