59f1181a1e42a2cad9b1c0ba139d66d7f4f6bbeb
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-12-04 07:34:46 +0900

    ok

--- src/script/setup.ts
+++ src/script/setup.ts
@@ -1,23 +1,24 @@
 import { filePath } from "/util/typedPath";
 
-const a: { host: Host; from: number }[] = [
+const a: { host: Host; from: number | "home" }[] = [
+  // { host: "n00dles", from: "home" },
   // { host: "foodnstuff", from: 0 },
   // { host: "sigma-cosmetics", from: 1 },
   // { host: "joesguns", from: 2 },
   // { host: "hong-fang-tea", from: 3 },
   // { host: "harakiri-sushi", from: 4 },
-  { host: "iron-gym", from: 5 },
-  { host: "zer0", from: 6 },
-  { host: "nectar-net", from: 7 },
-  { host: "max-hardware", from: 8 },
-  { host: "phantasy", from: 9 },
-  { host: "omega-net", from: 10 },
-  { host: "neo-net", from: 11 },
-  { host: "silver-helix", from: 12 },
-  { host: "crush-fitness", from: 13 },
-  { host: "computek", from: 14 },
-  { host: "the-hub", from: 15 },
-  { host: "johnson-ortho", from: 16 },
+  // { host: "iron-gym", from: 5 },
+  // { host: "zer0", from: 6 },
+  // { host: "nectar-net", from: 7 },
+  // { host: "max-hardware", from: 8 },
+  // { host: "phantasy", from: 9 },
+  // { host: "omega-net", from: 10 },
+  // { host: "neo-net", from: 11 },
+  // { host: "silver-helix", from: 12 },
+  // { host: "crush-fitness", from: 13 },
+  // { host: "computek", from: 14 },
+  // { host: "the-hub", from: 15 },
+  // { host: "johnson-ortho", from: 16 },
 ];
 
 export async function main(ns: NS): Promise<void> {
