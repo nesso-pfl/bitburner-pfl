3b943db585d3155cf9127c314fb252560da2985a
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-12-04 21:04:27 +0900

    ok

--- src/lib/tera/buyTera.ts
+++ src/lib/tera/buyTera.ts
@@ -1,4 +1,5 @@
-const PURCHASE_LATE = 0.5;
+const PURCHASE_RATE = 0.9;
+const UPGRADE_RATE = 0.5;
 const INITIAL_PURCHASE_RAM = 2 ** 12;
 const purchaseMessage = (tera: string) => `Purchase ${tera}\n`;
 const upgradeMessage = (teras: string[]) => `Upgrade ${teras.map((tera) => tera.substring(4)).join(", ")}`;
@@ -7,7 +8,7 @@ export const buyTera = async (ns: NS) => {
   const ownedTera = ns.getPurchasedServers();
   const shouldPurchaseTera =
     ownedTera.length < ns.getPurchasedServerLimit() &&
-    ns.getServerMoneyAvailable("home") * PURCHASE_LATE > ns.getPurchasedServerCost(INITIAL_PURCHASE_RAM);
+    ns.getServerMoneyAvailable("home") * PURCHASE_RATE > ns.getPurchasedServerCost(INITIAL_PURCHASE_RAM);
   if (shouldPurchaseTera) {
     ns.purchaseServer(`tera${ownedTera.length}`, INITIAL_PURCHASE_RAM);
   }
@@ -17,7 +18,7 @@ export const buyTera = async (ns: NS) => {
       (prev_, cur_) => prev_ + ns.getPurchasedServerUpgradeCost(cur_, ns.getServerMaxRam(cur_) * 2),
       0,
     );
-    return ns.getServerMoneyAvailable("home") * PURCHASE_LATE > upgradeCost ? [...prev, cur] : prev;
+    return ns.getServerMoneyAvailable("home") * UPGRADE_RATE > upgradeCost ? [...prev, cur] : prev;
   }, []);
   shouldUpgradeTeras.forEach((server) => ns.upgradePurchasedServer(server, ns.getServerMaxRam(server) * 2));
 
