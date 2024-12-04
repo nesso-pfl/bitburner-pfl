86777499ef437b29816a6a8e7cf998548a625b54
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-12-05 01:13:01 +0900

    ok

--- src/util/repeat.ts
+++ src/util/repeat.ts
@@ -3,12 +3,17 @@ type Option = Partial<{
   until: (ns: NS) => boolean;
 }>;
 
-export const repeat = async (ns: NS, fn: () => unknown, duration: number, option?: Option): Promise<void> => {
-  let count = option?.count ?? Infinity;
+export const repeat = async (
+  ns: NS,
+  fn: (count: number) => unknown,
+  duration: number,
+  option?: Option,
+): Promise<void> => {
+  let count = 0;
   while (true) {
-    await fn();
-    count--;
-    if (count < 1 || option?.until?.(ns)) return;
+    await fn(count);
+    count++;
+    if (count >= (option?.count ?? Infinity) || option?.until?.(ns)) return;
     await ns.sleep(duration);
   }
 };
