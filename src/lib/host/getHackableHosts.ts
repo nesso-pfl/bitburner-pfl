35805f197682d61f7e373aecedb3ca00157e3b3a
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-12-06 01:00:51 +0900

    ok

--- src/lib/host/getHackableHosts.ts
+++ src/lib/host/getHackableHosts.ts
@@ -1,5 +1,7 @@
 import { getAllHosts } from "/lib/host/getAllHosts";
 
+const blacklist = ["darkweb", "w0r1d_d43m0n"];
+
 export const getHackableHosts = (ns: NS) => {
-  return getAllHosts(ns).filter((host) => ns.getServer(host).hasAdminRights);
+  return getAllHosts(ns).filter((host) => ns.getServer(host).hasAdminRights && !blacklist.includes(host));
 };
