523dcb249acbca6e103305e2e5632330ec9a302d
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-12-06 01:32:43 +0900

    ok

--- src/script/hackDaemon.ts
+++ src/script/hackDaemon.ts
@@ -5,16 +5,5 @@ import { repeat } from "/util/repeat";
 export async function main(ns: NS): Promise<void> {
   const host = toHost(arg(ns.args[0], "string"));
   const duration = arg(ns.args[1], "number", 0);
-  let start = performance.now();
-  await repeat(
-    ns,
-    async () => {
-      ns.tprint(`s:Hack ${performance.now() - start}`);
-      await ns.hack(host);
-      const now = performance.now();
-      ns.tprint(`Hack ${now - start}`);
-      start = now;
-    },
-    duration,
-  );
+  await repeat(ns, async () => await ns.hack(host), duration);
 }
