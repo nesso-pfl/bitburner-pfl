5af908a180828519775bf9988c0f0fc8db76f6f9
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-12-05 13:47:12 +0900

    ok

--- src/script/earnMoney.ts
+++ src/script/earnMoney.ts
@@ -1,13 +1,7 @@
-import { earnMoney } from "/lib/host/earnMoney";
+import { earnMoney } from "/lib/host/earnMoney/index";
 import { toHost } from "/lib/host/toHost";
 import { arg } from "/util/arg";
 
 export async function main(ns: NS): Promise<void> {
-  const host = toHost(arg(ns.args[0], "string"));
-  if (ns.args[1] === "home") {
-    await earnMoney(ns, host, "home");
-  } else {
-    const teraIndex = arg(ns.args[1], "number");
-    await earnMoney(ns, host, `tera${teraIndex}`);
-  }
+  await earnMoney(ns, toHost(arg(ns.args[0], "string")));
 }
