2e26197c71beaa0d2024f00c6b1e7a7dc29b6731
nesso <32949286+nesso-pfl@users.noreply.github.com>
2024-12-06 01:00:43 +0900

    ok

--- src/script/showHostStats.ts
+++ src/script/showHostStats.ts
@@ -1,8 +1,24 @@
 import { getHackableHosts } from "/lib/host/getHackableHosts";
 import { getHostStats } from "/lib/host/getHostStats";
+import { toHost } from "/lib/host/toHost";
+import { getAllTeras } from "/lib/tera/getAllTeras";
 import { tprintTable } from "/util/tprintTable";
+import { filePath } from "/util/typedPath";
 
 export async function main(ns: NS): Promise<void> {
+  const processes = ([...getHackableHosts(ns), ...getAllTeras(ns)] satisfies (Host | Tera)[]).flatMap(ns.ps);
+  const showEarnMoneyStatus = (host: Host) => {
+    return !processes.find(
+      (process) => process.filename === filePath.script.earnMoney.$path && process.args[0] === host,
+    )
+      ? "N"
+      : processes.find((process) => process.filename === filePath.script.weakenDaemon.$path && process.args[0] === host)
+        ? "W"
+        : processes.find((process) => process.filename === filePath.script.growDaemon.$path && process.args[0] === host)
+          ? "G"
+          : "H";
+  };
+
   const data = getHackableHosts(ns)
     .map((host) => getHostStats(host, ns))
     .map((stat) => ({
@@ -11,6 +27,7 @@ export async function main(ns: NS): Promise<void> {
       maxMoney: stat.moneyMax ?? Infinity,
       availableRam: ns.formatRam(stat.availableRam),
       secLv: ((stat.hackDifficulty ?? 0) - (stat.minDifficulty ?? 0)).toFixed(2),
+      ems: showEarnMoneyStatus(toHost(stat.hostname)),
     }));
-  tprintTable(ns, data, ["name", "maxMoney", "money", "secLv"]);
+  tprintTable(ns, data, ["name", "maxMoney", "money", "secLv", "ems"]);
 }
